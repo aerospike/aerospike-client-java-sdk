5eb4f38e05923e37f813e4a140ad9e2c02ea4b3e
Brian Nichols <bnichols@aerospike.com>
2026-07-29 15:34:36 -0400

    CLIENT-5182 Cleanup tests. Fix warnings. Implement new tests where underlying functionality now exists. Delete obsolete tests.

--- client/src/test/java/com/aerospike/client/sdk/CdtMapKeyValueReadOrderTest.java
+++ client/src/test/java/com/aerospike/client/sdk/CdtMapKeyValueReadOrderTest.java
@@ -19,7 +19,6 @@ package com.aerospike.client.sdk;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.Map;
 import java.util.Set;
 
 import org.junit.jupiter.api.DisplayName;
--- client/src/test/java/com/aerospike/client/sdk/query/QueryHintTest.java
+++ client/src/test/java/com/aerospike/client/sdk/query/QueryHintTest.java
@@ -19,7 +19,6 @@ package com.aerospike.client.sdk.query;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.function.Function;
